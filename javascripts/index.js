4bc75fe9d7b0f683af6d4d45ab09987f7986c07e
flines <flines@126.com>
2016-11-03 15:37:22 +0800

    Create index.js

---
+++ javascripts/index.js
@@ -0,0 +1,41 @@
+define(function(require, exports, module){
+	var $ = require("jquery");
+	var _ = require("ramda");
+
+	var Impure = {
+      getJSON: _.curry(function(callback, url) {
+        $.getJSON(url, callback);
+      }),
+
+      setHtml: _.curry(function(sel, html) {
+        $(sel).html(html);
+      })
+    };
+
+    var img = function (url) {
+      return $('<img />', { src: url });
+    };
+
+    var trace = _.curry(function(tag, x) {
+      console.log(tag, x);
+      return x;
+    });
+
+    ////////////////////////////////////////////
+
+    var url = function (t) {
+      return 'https://flines.github.io/javascripts/' + t + '.json';
+    };
+
+    var mediaUrl = _.compose(_.prop('m'), _.prop('media'));
+
+    var srcs = _.compose(_.map(mediaUrl), _.prop('items'));
+
+    var images = _.compose(_.map(img), srcs);
+
+    var renderImages = _.compose(Impure.setHtml("body"), images);
+
+    var app = _.compose(Impure.getJSON(renderImages), url);
+
+    app("cats");
+});
